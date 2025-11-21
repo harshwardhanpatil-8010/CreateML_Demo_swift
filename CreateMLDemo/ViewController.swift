e6d34b45fc56a525fff4117b65bc35df18471fa4
SDC-USER <sdc-user@VY-7F-IOSTC-022.local>
2025-11-20 20:22:36 -0800

    new

---
+++ CreateMLDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreateMLDemo
+//
+//  Created by SDC-USER on 11/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
